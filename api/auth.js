b6b1f3b81e9fa888cff02f8cf6f2758f9d7b064c
Brandon Norris <bsn2qg@virginia.edu>
2023-01-09 12:04:15 -0500

    auth.js completed deleteRoutine

---
+++ api/auth.js
@@ -0,0 +1,217 @@
+// This api call helps to register a user, it is used in the register component 
+export const registerUser = async (username, password) => {
+  try {
+      const response = await fetch('http://fitnesstrac-kr.herokuapp.com/api/users/register', 
+      {
+          method: "POST",
+          headers: {
+              'Content-Type': 'application/json'
+          },
+      body: JSON.stringify({
+          user: {
+              username,
+              password,
+  }
+})
+});
+
+const {data: { token }} = await response.json();
+  return token;
+
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+
+
+
+export const loginUser = async (username, password) => {
+    try {
+        const response = await fetch('http://fitnesstrac-kr.herokuapp.com/api/users/login', 
+        {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                user: {
+                    username,
+                    password,
+                }
+            })
+        });
+        
+        const {data: { token }} = await response.json();
+        return token;
+        
+    } catch (error) {
+        console.log(error);
+    }
+};
+export const fetchMe = async (token) => {
+  try {
+    const response = await fetch ('http://fitnesstrac-kr.herokuapp.com/api/users/me', 
+    {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    },
+  })
+
+  const { data } = await response.json();
+  console.log("This is me object: ", data)
+  return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+// Get/Post/Delete requests
+
+export const getRoutinesByUsername = async (username) => {
+  
+    fetch(`http://fitnesstrac-kr.herokuapp.com/api/users/${username}/routines`)
+    .then(response => response.json())
+    .then(result => {
+      console.log(result)
+    })
+    .catch(console.error);
+  }
+
+
+export const getActivities = async () => {
+    fetch('http://fitnesstrac-kr.herokuapp.com/api/activities', {
+  headers: {
+    'Content-Type': 'application/json',
+  },
+}).then(response => response.json())
+  .then(result => {
+    console.log(result);
+  })
+  .catch(console.error);
+}
+
+
+
+// !check to verify this is working
+export const createActivity = async (token, name, description) => {
+    fetch('http://fitnesstrac-kr.herokuapp.com/api/activities', {
+        method: "POST", 
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            Activity: {name, description}
+  }),
+}).then(response => response.json())
+  .then(result => {
+    console.log(result);
+  })
+  .catch(console.error);
+}
+
+// PATCH /api/activities/:activityId (*)
+export const patchActivity = async (token, name, description, id) => {
+    fetch(`http://fitnesstrac-kr.herokuapp.com/api/activities/${id}`, {
+  method: "PATCH",
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${token}`
+  },
+  body: JSON.stringify({
+    Activity: {name, description}
+  })
+}).then(response => response.json())
+  .then(result => {
+    console.log(result);
+  })
+  .catch(console.error);
+}
+
+// GET /api/activities/:activityId/routines
+export const getRoutineByActivityId = async (activityId) => {
+    fetch(`http://fitnesstrac-kr.herokuapp.com/api/activities/${activityId}/routines`, {
+  headers: {
+    'Content-Type': 'application/json',
+  },
+}).then(response => response.json())
+  .then(result => {
+    console.log(result);
+  })
+  .catch(console.error);
+}
+
+// GET /api/routines
+export const getRoutines = async () => {
+    fetch('http://fitnesstrac-kr.herokuapp.com/api/routines', {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then(response => response.json())
+    .then(result => {
+      console.log(result);
+    })
+    .catch(console.error);
+}
+
+// POST /api/routines (*)
+// !Double check here
+export const createRoutine = async (token, name, goal, isPublic) => {
+ fetch('http://fitnesstrac-kr.herokuapp.com/api/routines', {
+  method: "POST",
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${token}`
+  },
+  body: JSON.stringify({
+    Routine: {name, goal, isPublic}
+  })
+}).then(response => response.json())
+  .then(result => {
+    console.log(result);
+  })
+  .catch(console.error);
+}
+
+// PATCH /api/routines/:routineId (**)
+// ?how do we check for user only? 
+
+export const patchRoutine = async (token, name, goal, isPublic, routineId) => {
+    fetch(`http://fitnesstrac-kr.herokuapp.com/api/routines/${routineId}`, {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          },
+        body: JSON.stringify({
+          Routine: {name, goal, isPublic}
+        })
+      }).then(response => response.json())
+        .then(result => {
+          console.log(result);
+        })
+        .catch(console.error);
+}
+
+// DELETE /api/routines/:routineId (**)
+// ? need to be user specific here as well 
+
+export const deleteRoutine = async (token, routineId) => {
+    fetch(`http://fitnesstrac-kr.herokuapp.com/api/routines/${routineId}`, {
+  method: "DELETE",
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${token}`
+  }
+}).then(response => response.json())
+  .then(result => {
+    console.log(result);
+  })
+  .catch(console.error);
+}
+
+// POST /api/routines/:routineId/activities
+export const create
---
+++ app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Login from "./components/Login-out";
+import Nav from "./components/Nav";
+import Register from "./components/Register";
+import UserInfo from "./components/UserInfo";
+import { route, routes } from "react-router-dom";
+
+const App = () => {
+  return <div></div>;
+};
+
+export default App;
